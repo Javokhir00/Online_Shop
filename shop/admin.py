0aeb7fa23ac23d9442490c648940424b435ab489
Javohir <your_email>
2025-05-08 14:56:40 +0500

    dfsd

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import Product, Category
 from django.contrib.auth.models import User,Group
+
 # Register your models here.
 
-# admin.site.register(Product)
+admin.site.register(Product)
 
 admin.site.register(Category)
 
